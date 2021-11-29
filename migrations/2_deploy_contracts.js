5a68938891f33eadedd92b04adb50407e9455971
Aliceonly <1139045289@qq.com>
2021-11-29 19:44:51 +0800

    develop of sol/truffle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var EcommerceStore_chenjunjie = artifacts.require("./EcommerceStore_chenjunjie.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(EcommerceStore_chenjunjie);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -8,5 +8,10 @@ module.exports = {
       port: 8545,
       network_id: '*' // Match any network id
     }
+  },
+  compilers: {
+    solc: {
+      version: "./solc/soljson-v0.8.0+commit.c7dfd78e.js"
+    }
   }
 }
